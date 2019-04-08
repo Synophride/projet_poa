824eed66d4df51a263a5b0c3be99714b990a27ed
Julien Guyot <jguyot2@a-018818.ups.u-psud.fr>
2019-04-08 11:06:39 +0200

    feazef

--- LabyrintheProto/Gardien.cc
+++ LabyrintheProto/Gardien.cc
@@ -1,14 +1,30 @@
 #include "Gardien.h"
 
+#include <cstdlib>
+#include <cmath>
 
 void Gardien::update(void){
-    move((double) (rand()%100) / 100. , ((double) (rand()%100) / 100. ));
+    // move((double) (rand()%100) / 100. , ((double) (rand()%100) / 100. ));
+    float speed = 1.;
+/*    bool moved =
+	   move(speed * std::cos(( _angle) /180),
+		speed * std::sin(( _angle) /180))
+      	|| move(speed * std::cos(((( 180 + _angle)%180) /180 )),
+	speed * std::sin(((( 180 + _angle)%180) /180 )));*/
+    double dx = (double)(rand()%10 - 5) / 10.,
+	   dy = (double)(rand()%10 - 5) / 10.;
+    move(dx, dy);
+    // Todo 
 }
 
 bool Gardien::is_legit_move(double dx, double dy){
-    return EMPTY == _l -> data(
-		(int)((_x + dx) / Environnement::scale),
-		(int)((_y + dy) / Environnement::scale));
+    int new_x = (int)((_x + dx) / Environnement::scale);
+    int new_y = (int)((_y + dy) / Environnement::scale);
+    return (new_x >= 0 
+	    && new_x < _l->width()
+	    && new_y >= 0
+	    && new_y < _l->height()
+	    && (EMPTY == _l -> data(new_x, new_y)));
 }
 
 bool Gardien::try_move(double dx, double dy ){
@@ -16,11 +32,12 @@ bool Gardien::try_move(double dx, double dy ){
 	return false;
     _x += dx;
     _y += dy;
-    return true;    
+    _angle = std::atan2(dx, dy) * 180;
+    return true;
 }
 
 bool Gardien::move(double dx, double dy){
-    return try_move(dx, dy);// || try_move(dx, 0.0) || try_move(0.0, dy);
+    return try_move(dx, dy) || try_move(dx, 0.0) || try_move(0.0, dy);
 }
 
 void Gardien::fire(int angle_vertical){
